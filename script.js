d0e35022ab8f23b1eb4b3230f998ed6b201b3076
Tejas Kumar S <tej0897@gmail.com>
2024-05-11 22:09:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,10 +11,16 @@ const getMedian = (array) => {
 
 const getMode = (array) => {
   const counts = {};
-  array.forEach(el => counts[el] = (counts[el] || 0) + 1);
-  if(new Set(Object.values(counts)).size === 1){
+  array.forEach((el) => {
+    counts[el] = (counts[el] || 0) + 1;
+  })
+  if (new Set(Object.values(counts)).size === 1) {
     return null;
   }
+  const highest = Object.keys(counts).sort(
+    (a, b) => counts[b] - counts[a]
+  )[0];
+
 }
 
 
