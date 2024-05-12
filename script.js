5f557fa5ee0180fe85fa2e8d60792bb866460daf
Tejas Kumar S <tej0897@gmail.com>
2024-05-12 09:26:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -36,21 +36,26 @@ const getVariance = (array) => {
     const difference = el - mean;
     const squared = difference ** 2;
     return acc + squared;
-  }, 0);
+  }, 0) / array.length;
+  return variance;
 }
+
+
 
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
   const array = value.split(/,\s*/g);
   const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
-
+  
   const mean = getMean(numbers);
   const median = getMedian(numbers);
   const mode = getMode(numbers);
   const range = getRange(numbers);
+  const variance = getVariance(numbers);
 
   document.querySelector("#mean").textContent = mean;
   document.querySelector("#median").textContent = median;
   document.querySelector("#mode").textContent = mode;
   document.querySelector("#range").textContent = range;
+  document.querySelector("#variance").textContent = variance;
 }
