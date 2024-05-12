35315cd7b846d3cbff81a297feaba41b9eaa7fd6
Tejas Kumar S <tej0897@gmail.com>
2024-05-12 09:32:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -40,7 +40,11 @@ const getVariance = (array) => {
   return variance;
 }
 
-
+const getStandardDeviation = (array) => {
+  const variance = getVariance(array);
+  const standardDeviation = Math.sqrt(variance);
+  return standardDeviation;
+}
 
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
@@ -52,10 +56,12 @@ const calculate = () => {
   const mode = getMode(numbers);
   const range = getRange(numbers);
   const variance = getVariance(numbers);
+  const standardDeviation = getStandardDeviation(numbers);
 
   document.querySelector("#mean").textContent = mean;
   document.querySelector("#median").textContent = median;
   document.querySelector("#mode").textContent = mode;
   document.querySelector("#range").textContent = range;
   document.querySelector("#variance").textContent = variance;
+  document.querySelector("#standardDeviation").textContent = standardDeviation;
 }
